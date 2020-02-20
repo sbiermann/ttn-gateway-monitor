b9313a5e8519e378540da90fa1a6d96d7a1b37f6
sbn <sb@ems-solutions.com>
2020-02-20 21:19:41 +0100

    switching to ejb and using schedule for cleaning up slack session

--- src/main/java/com/ems/ttngwmonitor/slack/SlackService.java
+++ src/main/java/com/ems/ttngwmonitor/slack/SlackService.java
@@ -1,98 +1,91 @@
 package com.ems.ttngwmonitor.slack;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.apache.deltaspike.core.api.config.ConfigProperty;
-import org.glassfish.grizzly.http.server.util.SimpleDateFormats;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.ems.ttngwmonitor.ttn.res.Gateway;
 import com.ullink.slack.simpleslackapi.SlackChannel;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+import org.apache.deltaspike.core.api.config.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 
-@ApplicationScoped
-public class SlackService
-{
-	private final static Logger logger = LoggerFactory.getLogger( SlackService.class );
+@Singleton
+public class SlackService {
+    private final static Logger logger = LoggerFactory.getLogger(SlackService.class);
 
-	private SlackSession session;
+    private SlackSession session;
 
-	private HashMap<String, Date> map = new HashMap();
+    private HashMap<String, Date> map = new HashMap();
 
-	@Inject
-	@ConfigProperty(name = "slack.token")
-	private String slackToken;
+    private long lastAccess = 0;
 
-	@PostConstruct
-	public void postConstruct()
-	{
-		session = SlackSessionFactory
-				.getSlackSessionBuilder( slackToken )
-				.withAutoreconnectOnDisconnection(true)
-				.withConnectionHeartbeat(10, TimeUnit.SECONDS)
-				.build();
-		try
-		{
-			session.connect();
-		}
-		catch( IOException e )
-		{
-			logger.error( "error while connecting to slack" );
-		}
-	}
+    @Inject
+    @ConfigProperty(name = "slack.token")
+    private String slackToken;
+
+    @PostConstruct
+    public void postConstruct() {
+        session = SlackSessionFactory
+                .getSlackSessionBuilder(slackToken)
+                .build();
+    }
 
 
-	public void informChannelForGateway( Gateway gateway, Date date )
-	{
-		try
-		{
-			if(map.containsKey( gateway.getId() ))
-			{
-				Date lastMessageDate = map.get( gateway.getId() );
-				Calendar now = Calendar.getInstance();
-				now.add( Calendar.DATE, -1 );
-				if(lastMessageDate.after( now.getTime() ))
-					return;
-			}
-			map.put( gateway.getId(), new Date() );
-			if( !session.isConnected() )
-				session.connect();
-			SlackChannel channel = session.findChannelByName( "gatewaymonitoring" ); //make sure bot is a member of the channel.
-			String message = String.format( "Gateway: %1$s (%2$s) ist zu lange offline, letzte online Meldung: %3$td.%3$tm.%3$ty %3$tT", gateway.getId(), gateway.getOwner(), date );
-			session.sendMessage( channel, message);
-		}
-		catch( IOException e )
-		{
-			logger.error( "error while connecting to slack" );
-		}
+    public void informChannelForGateway(Gateway gateway, Date date) {
+        try {
+            if (map.containsKey(gateway.getId())) {
+                Date lastMessageDate = map.get(gateway.getId());
+                Calendar now = Calendar.getInstance();
+                now.add(Calendar.DATE, -1);
+                if (lastMessageDate.after(now.getTime()))
+                    return;
+            }
+            map.put(gateway.getId(), new Date());
+            lastAccess = System.currentTimeMillis();
+            if (!session.isConnected())
+                session.connect();
+            SlackChannel channel = session.findChannelByName("gatewaymonitoring"); //make sure bot is a member of the channel.
+            String message = String.format("Gateway: %1$s (%2$s) ist zu lange offline, letzte online Meldung: %3$td.%3$tm.%3$ty %3$tT", gateway.getId(), gateway.getOwner(), date);
+            session.sendMessage(channel, message);
+        } catch (IOException e) {
+            logger.error("error while connecting to slack");
+        }
 
-	}
+    }
 
-	public void informChannelForReturningGateway(Gateway gateway, Date date) {
-		try
-		{
-			if( !session.isConnected() )
-				session.connect();
-			SlackChannel channel = session.findChannelByName( "gatewaymonitoring" ); //make sure bot is a member of the channel.
-			String message = String.format( "Gateway: %1$s (%2$s) ist wieder seid %3$td.%3$tm.%3$ty %3$tT online", gateway.getId(), gateway.getOwner(), date );
-			session.sendMessage( channel, message);
-		}
-		catch( IOException e )
-		{
-			logger.error( "error while connecting to slack" );
-		}
-	}
+    public void informChannelForReturningGateway(Gateway gateway, Date date) {
+        lastAccess = System.currentTimeMillis();
+        try {
+            if (!session.isConnected())
+                session.connect();
+            SlackChannel channel = session.findChannelByName("gatewaymonitoring"); //make sure bot is a member of the channel.
+            String message = String.format("Gateway: %1$s (%2$s) ist wieder seid %3$td.%3$tm.%3$ty %3$tT online", gateway.getId(), gateway.getOwner(), date);
+            session.sendMessage(channel, message);
+        } catch (IOException e) {
+            logger.error("error while connecting to slack");
+        }
+    }
+
+    @Schedule(hour = "*", minute = "*/6", persistent = false)
+    public void checkForSessionOpen() {
+        if (lastAccess <= 0)
+            return;
+        try {
+            if (System.currentTimeMillis() - lastAccess > TimeUnit.MINUTES.toMillis(5))
+                session.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
